232103360a1b4b9914aac314511334fa91638c67
SeongeunYang <yse09@naver.com>
2021-12-27 18:27:18 +0900

    자유 게시물 이미지 예외 처리

--- src/routes/FreePost.js
+++ src/routes/FreePost.js
@@ -6,8 +6,10 @@ function FreePost() {
     const AWS = "http://13.125.249.172";
     const skiResort = "HighOne";
     const tokenHeader =
-        { "Authorization": localStorage.getItem('token') }
-    const [img, setImg] = useState([]);
+        { "Authorization": localStorage.getItem('token') };
+    
+    const emptyFile = new File([""], "empty");
+    const [img, setImg] = useState(emptyFile);
     const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
 
@@ -44,9 +46,9 @@ function FreePost() {
 
     return (
         <div>
-            <input type="file" onChange={uploadImg} />
-            <input type="text" onChange={uploadTitle} />
-            <input type="text" onChange={uploadContent} />
+            <p><input type="file" onChange={uploadImg} /></p>
+            <p>제목 : <input type="text" onChange={uploadTitle} /></p>
+            <p>내용 : <input type="text" onChange={uploadContent} /></p>
             <button onClick={onClick}>보내기</button>
         </div>
     );
